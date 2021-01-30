81c16e0d77519deb4426b2e564a0bcb0b043e072
gyeong <gyeong5961@gmail.com>
2021-01-30 22:57:29 +0900

    2021/01/29 chapter13 succeed

--- docker/mysql.sql
+++ docker/mysql.sql
@@ -4,4 +4,6 @@ CREATE SCHEMA nodebird character set utf8;
 GRANT ALL privileges ON nodebird.* TO nodebird@'%' IDENTIFIED BY 'nodebird1234567';
 CREATE SCHEMA nodebird_test character set utf8;
 GRANT ALL privileges ON nodebird_test.* TO nodebird@'%' IDENTIFIED BY 'nodebird1234567';
+CREATE SCHEMA nodeauction character set utf8;
+GRANT ALL privileges ON nodeauction.* TO nodeauction@'%' IDENTIFIED BY 'nodeauction1234567';
 flush tables;flush privileges;
